ac96872cf961cda3bd1e724567a6fbb1d8856dcd
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-25 20:08:40 +0000

    Parameterise tests

--- main_test.py
+++ main_test.py
@@ -1,5 +1,11 @@
+import pytest
 from main import get_grid_size
 
-def test_get_grid_size():
-    assert get_grid_size(9) == 3
-    assert get_grid_size(16) == 4
+grid_size_data = [
+    (9, 3),
+    (16, 4)
+]
+
+@pytest.mark.parametrize("data_size,expected", grid_size_data)
+def test_get_grid_size(data_size, expected):
+    assert get_grid_size(data_size) == expected
