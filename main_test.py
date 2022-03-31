1bf5764debbc49d07d01fa15e238df636c1867d9
Alison Clarke <alison.r.clarke@durham.ac.uk>
2022-03-31 19:51:38 +0100

    Add extra tests and fix function

--- main_test.py
+++ main_test.py
@@ -2,8 +2,10 @@ import pytest
 from main import get_grid_size
 
 grid_size_data = [
-    (9, 3),
-    (16, 4)
+    (9, 3),  # square number
+    (16, 4), # square number
+    (10, 4), # 1 bigger than square - should round up
+    (15, 4)  # 1 smaller than square - should round up
 ]
 
 @pytest.mark.parametrize("data_size,expected", grid_size_data)
